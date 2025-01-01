06e1a23ea9e45fb66522bdc8f1124db91dd9b868
waizin28 <waizinlinn.08@gmail.com>
2024-12-31 19:27:11 -0600

    useRef input

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,20 +1,31 @@
-import React, { type FormEvent } from 'react';
+import React, { useRef, type FormEvent } from 'react';
 
 const NewGoal = () => {
-  function handleSubmit(event: FormEvent) {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    // extractin balue of form data
+    // we can extract input data using this
+    // new FormData(event.currentTarget)
+
+    // to express never be null with !
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor='goal'>Your goal</label>
-        <input id='goal' type='text' />
+        <input id='goal' type='text' ref={goal} />
       </p>
 
       <p>
         <label htmlFor='summary'>Short summary</label>
-        <input id='summary' type='text' />
+        <input id='summary' type='text' ref={summary} />
       </p>
 
       <p>
